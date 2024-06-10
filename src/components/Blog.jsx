d88daff50f0868b71bf96eea3a8dcc3148ae724a
kurkilmo <ilmo.kurki00@gmail.com>
2024-06-10 22:10:15 +0300

    Tehtävä 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [ expanded, setExpanded] = useState(false)
+
+  const handleLike = (event) => {
+    event.preventDefault()
+    console.log(`Liked ${blog.title}`)
+  }
+
+  const extraInfo = () => {
+    if (!expanded) return null
+    return (
+      <div>
+        <a href={blog.url}>{blog.url}</a> <br/>
+        {blog.likes}
+        <button onClick={handleLike}>like</button> <br />
+        {blog.user.name} <br/>
+      </div>
+    )
+  }
+
+  const buttonText = expanded
+    ? 'hide'
+    : 'view'
+
+  return (
+    <div className='blog'>
+      {blog.title} - {blog.author}
+      <button onClick={() => setExpanded(!expanded)}>{buttonText}</button>
+      {extraInfo()}
+    </div>
+  )
+}
 
 export default Blog
